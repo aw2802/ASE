c2f1cd01e24acc3e46c17c9abae6a26fa9557d55
Tin Nilar Hlaing <tinnilarh@gmail.com>
2016-10-04 21:14:33 -0400

    old python file

--- server.py
+++ server.py
@@ -9,27 +9,9 @@ conn = pymssql.connect(server='eats.database.windows.net',
 
 @app.route("/")
 def main():
-  # conn = pymssql.connect(server='eats.database.windows.net', 
-  #   user='th2520@eats', 
-  #   password='123*&$eats', 
-  #   database='AERIS') 
-  print ("connected!")
+
   cursor = conn.cursor()  
 
-  # cursor.executemany(
-  # "INSERT INTO Contacts VALUES (%s, %d)",
-  # [('Anna Wen', 1234567890),
-  # ('Tin Nilar', 4567123456),
-  # ('James Corden', 1235768970)])
-
- # while row:  
-  #     print ("Inserted Product ID : " +str(row[0]))  
-  #     row = cursor.fetchone() 
-
-  # while row: 
-  #   print ("Name=%s, Number=%d" % (row[0], row[1]))
-  #   render_template('index.html', name = row[0], number = row[1])
-  #   row = cursor.fetchone()
   
   cursor.execute("SELECT * FROM Contacts")
   data= cursor.fetchall()  
@@ -93,7 +75,7 @@ def delete():
 
 
   return render_template('index.html', data= dataTable)
-  conn.close()
+  # conn.close()
 
 if __name__ == "__main__":
   app.debug = True
